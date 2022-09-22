5cb65de4046237bcee620cd6c5808f33fb131582
jdane <julien.dane@gmail.com>
2022-09-22 16:53:44 +0200

    Implémenté cloneArray et nPremiersElements

--- 01_cloneArray/cloneArray.js
+++ 01_cloneArray/cloneArray.js
@@ -1,3 +1,45 @@
 /* 1/ Écrivez une fonction JavaScript pour cloner un tableau.
 Ex : array_clone([1,2,3,4])
 Sortie prévue : [1,2,3,4] */
+
+// essayons avec slice
+
+function cloneArray(tableau) {
+  return tableau.slice();
+}
+
+// tests en dur
+
+// utilitaires
+
+function compareValeursArray(array1, array2) {
+  /* on est obligé de comparer les valeurs une à une
+  pour s'assurer que le second Array est une copie
+  fidèle du premier */
+  
+  if (array1.length !== array2.length) {
+    return false;
+  }
+
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    } 
+  }
+
+  return true;
+}
+
+function log (a) {
+  const b = cloneArray(a);
+  console.log(
+    `référence de [${a}] != référence de [${b}] ? ${a !== b}\n` +
+    `valeurs de [${a}] = valeurs de [${b}] ? ${compareValeursArray(a, b)}`
+  );
+}
+
+// tests
+
+log([1, 2, 3, 4]);
+log([]);
+log(["youpi", "banane", "slip", "man"]);
--- 02_nPremiersElements/nPremiersElements.js
+++ 02_nPremiersElements/nPremiersElements.js
@@ -3,3 +3,30 @@ tableau. Passer un paramètre 'n', retournera les 'n' premiers éléments du
 tableau
 Ex : first([7, 9, 0, -2],3)
 Sortie prévue : 7,9,0 */
+
+// utilisons slice !
+
+function nPremiersElements(array, n) {
+  // mais retournons un tableau vide si n nous fait sortir de l'array
+  if (n < 0 || n > array.length) {
+    return [];
+  }
+
+  return array.slice(0, n);
+}
+
+//tests manuels
+
+function log(array, n) {
+  console.log(
+    `${n} premiers éléments de [${array}] => [${nPremiersElements(array, n)}]`
+  );
+}
+
+log([7, 9, 0, -2], 3);
+log([7, 9, 0, -2], 0);
+log([7, 9, 0, -2], 1);
+log([7, 9, 0, -2], 2);
+log([7, 9, 0, -2], 4);
+log([7, 9, 0, -2], 5);
+log([7, 9, 0, -2], -1);
